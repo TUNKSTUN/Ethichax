e289a0b5577d883aea9f3ed58345818b2060a1ba
TUNKSTUN <johnwick4learning@gmail.com>
2023-02-25 09:26:01 +0530

    Ethichax Backend Project

--- src/main/java/com/ethic/backend/controller/ArticleController.java
+++ src/main/java/com/ethic/backend/controller/ArticleController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000", "https://2620:0:890::100"})
+@CrossOrigin("http://localhost:3000")
 public class ArticleController {
 
     @Autowired
